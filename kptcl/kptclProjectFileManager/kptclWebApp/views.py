a11136588eb2b402d139c6e713987b4c7d966cab
KushalGowdaa <kushallgowda97@gmail.com>
2024-09-08 18:14:57 +0530

    databse changes

--- kptcl/kptclProjectFileManager/kptclWebApp/views.py
+++ kptcl/kptclProjectFileManager/kptclWebApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
 def dashboard(request):
     return render(request, "dashboard/Contractor_dash.html", None)
+
